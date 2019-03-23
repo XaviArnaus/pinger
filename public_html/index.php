a511f57e3ef4889f9377f6b3b7f4f51a65ac3264
Xavier Arnaus <xavier@moon>
2019-03-23 12:32:01 +0100

    Reporting. Initial approach

--- public_html/index.php
+++ public_html/index.php
@@ -65,19 +65,105 @@ class Reader {
     }
 }
 
+class Render {
+
+    const DIRECTORY_PATH_MODIFICATOR = '..' . DIRECTORY_SEPARATOR;
+    const VALID = "#27AE60";
+    const INVALID = "#E74C3C";
+
+    private $results;
+    private $templates;
+
+    public function __construct() {
+        $this->templates = [
+            "layout"            => self::DIRECTORY_PATH_MODIFICATOR . "templates/layout.html",
+            "main_info"         => self::DIRECTORY_PATH_MODIFICATOR . "templates/main_info.html",
+            "service_result"    => self::DIRECTORY_PATH_MODIFICATOR . "templates/service_result.html",
+            "validation_result" => self::DIRECTORY_PATH_MODIFICATOR . "templates/validation_result.html",
+        ];
+    }
+
+    public function setResultsToDisplay(array $results) {
+        $this->results = $results;
+    }
+
+    public function show() {
+
+        $service_content = "";
+        foreach($this->results["service_results"] as $service_result) {
+            $params = [
+                "{%-NAME-%}" => $service_result["service_name"],
+                "{%-URL-%}" => $service_result["request"]["url"],
+                "{%-DURATION-%}" => $service_result["duration"],
+                "{%-ERROR-%}" => $service_result["error"] ? $service_result["error"] : "No",
+            ];
+            $validation_content = "";
+            foreach ($service_result["validation_results"] as $validation_result) {
+                $validation_content .= $this->renderTemplate(
+                    "validation_result",
+                    [
+                        "{%-CLASS-%}" => $validation_result["validation_class"],
+                        "{%-VALUE-%}" => $validation_result["valid_value"],
+                        "{%-RESULT_COLOR-%}" => boolval($validation_result["is_valid"]) ? self::VALID : self::INVALID
+                    ]
+                );
+            }
+            $service_content .= $this->renderTemplate(
+                "service_result",
+                array_merge($params, ["{%-VALIDATION RESULTS-%}" => $validation_content])
+            );
+        }
+
+        $main_info_content = $this->renderTemplate(
+            "main_info",
+            [
+                "{%-VERSION-%}" => $this->results["version"],
+                "{%-DATE-%}" => $this->results["date"],
+                "{%-DURATION-%}" => $this->results["duration"]
+            ]
+        );
+
+        $content = $this->renderTemplate(
+            "layout",
+            [
+                "{%-MAIN_INFO-%}" => $main_info_content,
+                "{%-RESULTS-%}" => $service_content
+            ]
+        );
+
+        print $content;
+    }
+
+    private function renderTemplate($template_name, $parameters = []) {
+        $content = $this->getTemplateContent($template_name);
+        foreach ($parameters as $key => $value) {
+            $content = str_replace($key,$value,$content);
+        }
+        return $content;
+    }
+
+    private function getTemplateContent($template_name) {
+        return file_get_contents($this->templates[$template_name]);
+    }
+
+}
+
 
 class App {
     const CONFIG_FILE = "../config.json";
     private $config;
     private $reader;
+    private $render;
 
     public function __construct() {
         $this->config = new Config(self::CONFIG_FILE);
         $this->reader = new Reader($this->config);
+        $this->render = new Render();
     }
 
     public function run() {
-        print $this->getLastResult();
+        $this->render->setResultsToDisplay(json_decode($this->getLastResult(), true));
+        $this->render->show();
     }
 
     private function getLastResult() {
